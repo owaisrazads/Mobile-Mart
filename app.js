4ca73319e65f09355f1149ce6b39c5253780b49f
unknown <oraza7969@gmail.com>
2023-10-03 19:56:54 -0700

    mart

---
+++ app.js
@@ -0,0 +1,173 @@
+const div = document.querySelector(".htmldiv");
+let img = document.createElement("img");
+const checkBoxDiv = document.querySelector("#checkBox");
+
+const phones = [
+  {
+    brand: "Samsung",
+    model: "S20",
+    image: "Galaxy s20 ultra",
+    ram: 8,
+    rom: 256,
+    camera: "20 megapixel",
+    price: "75000",
+  },
+  {
+    brand: "Xiomi",
+    model: "note10",
+    image: "xiaomi10.jpg",
+    ram: 4,
+    rom: 64,
+    camera: "10 megapixel",
+    price: "35000",
+  },
+  {
+    brand: "Infinix",
+    model: "z10",
+    image: "infinix10.jpg",
+    ram: 2,
+    rom: 16,
+    camera: "5 megapixel",
+    price: "15999",
+  },
+  {
+    brand: "Tecno",
+    model: "spark10",
+    image: "tecno spark 10.jpg",
+    ram: 12,
+    rom: 512,
+    camera: "25 megapixel",
+    price: "32000",
+  },
+  {
+    brand: "Iphone",
+    model: "14",
+    image: "iphone 14.webp",
+    ram: 4,
+    rom: 1024,
+    camera: "30 megapixel",
+    price: "380000",
+  },
+  {
+    brand: "Oppo",
+    model: "F11",
+    image: "oppo f11.jpg",
+    ram: 8,
+    rom: 256,
+    camera: "20 megapixel",
+    price: "56000",
+  },
+  {
+    brand: "Vivo",
+    model: "y20",
+    image: "vivo y20.png",
+    ram: 4,
+    rom: 64,
+    camera: "8 megapixel",
+    price: "29000",
+  },
+  {
+    brand: "Tecno",
+    model: "spark10",
+    image: "tecno spark 10.jpg",
+    ram: 12,
+    rom: 512,
+    camera: "25 megapixel",
+    price: "32000",
+  },
+  {
+    brand: "Iphone",
+    model: "14",
+    image: "iphone 14.webp",
+    ram: 4,
+    rom: 1024,
+    camera: "30 megapixel",
+    price: "380000",
+  },
+  {
+    brand: "Oppo",
+    model: "F11",
+    image: "oppo f11.jpg",
+    ram: 8,
+    rom: 256,
+    camera: "20 megapixel",
+    price: "56000",
+  },
+  {
+    brand: "Vivo",
+    model: "y20",
+    image: "vivo y20.png",
+    ram: 4,
+    rom: 64,
+    camera: "8 megapixel",
+    price: "29000",
+  },
+];
+
+for (let i = 0; i < phones.length; i++) {
+  div.innerHTML += `<div class="maindiv"><img src="./assets/${phones[i].image}" alt="">
+  <h1>${phones[i].brand} ${phones[i].model}</br>
+  Ram:${phones[i].ram} GB </br>
+  Rom:${phones[i].rom} </br>
+  Camera:${phones[i].camera} </br>
+  <span class="price">Price: ${phones[i].price}/= pkr</span></h1>
+  <button onclick="checkBox(${i})">Add To Cart</button></div>`;
+}
+
+const cartData = localStorage.getItem('cartArr')
+const jsonData = JSON.parse(cartData)
+const newArr = [...jsonData];
+
+function checkBox(index) {
+  if (newArr.includes(phones[index]) === true) {
+    for (let i = 0; i < newArr.length; i++) {
+      Swal.fire({
+        title: "Do you want to add to cart?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Add",
+        denyButtonText: `Don't Add`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Added!", "", "success");
+          if (newArr[i] === phones[index]) {
+            newArr[i].quantity += 1;
+            const items = JSON.stringify(newArr);
+            localStorage.setItem('cartArr' , items);
+            // console.log(newArr);
+          }
+        } else if (result.isDenied) {
+          Swal.fire("Order are not Added", "", "info");
+        }
+      });
+      
+    }
+  } else {
+    Swal.fire({
+      title: "Do you want to add to cart?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Add",
+      denyButtonText: `Don't Add`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Added!", "", "success");
+        phones[index].quantity = 1;
+        newArr.push(phones[index]);
+        console.log(newArr);
+        const items = JSON.stringify(newArr);
+        localStorage.setItem('cartArr' , items);
+      } else if (result.isDenied) {
+        Swal.fire("Order are not Added", "", "info");
+      }
+    });
+  }
+  
+
+}
+
+function goToCart() {
+  window.location = 'cart.html'
+}
---
+++ cart.js
@@ -0,0 +1,74 @@
+const data = localStorage.getItem('cartArr');
+const items = JSON.parse(data);
+console.log(items);
+
+function backToHome() {
+    window.location = 'index.html'
+    localStorage.setItem('cartArr' , JSON.stringify(items))
+}
+
+const div = document.querySelector('#itemsDiv')
+const totalAmounts = document.querySelector('.pera')
+
+
+function render() {
+    let totalAmount = 0;
+    totalAmounts.innerHTML = "";
+    if(items.length > 0){
+        for (let i = 0; i < items.length; i++) {
+            totalAmount += items[i].price * items[i].quantity
+            div.innerHTML += `<div class="maindiv"><img src="./assets/${items[i].image}" alt="">
+            <h1>${items[i].brand} ${items[i].model}</br>
+            Quantity:<button class = "operator" onclick = "sumValue(${i})">-</button>${items[i].quantity}<button class = "operator" onclick="addValue(${i})">+</button></br>
+            <span class="price">Price: ${items[i].price}/= pkr</span></br>
+            </h1>
+            </div>`;
+            totalAmounts.innerHTML = `Total Amount = ${totalAmount}/=`
+        }
+    } else{
+        div.innerHTML = `No Item Found ...`
+    }
+    
+}
+
+render()
+
+
+
+
+function addValue(index) {
+    div.innerHTML = "";
+    items[index].quantity += 1;
+    
+    render()
+}
+
+function sumValue(index) {
+    div.innerHTML = "";
+    items[index].quantity -= 1;
+    
+    render()
+    if (items[index].quantity === 0){
+        div.innerHTML = "";
+        items.splice(index , 1)
+        render();
+    }
+    
+    
+}
+
+// function totalprice() {
+//     para.innerHTML = ""
+//     let totalprice = 0
+//     for(i = 0; i < items.length; i++){
+//         // const itemtotal = items[i].totalprice
+//         totalprice += items[i].totalprice
+        
+//     }
+//     para.innerHTML= `<p>total price: ${totalprice}</p>`
+
+// }
+
+window.onbeforeunload = function() {
+    localStorage.setItem('cartArr' , JSON.stringify(items));
+};
